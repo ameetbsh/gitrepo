30831f69f9758fb8825cf6d672ca60ae1bd161fc
Ameet Bunsoogawah <ameetbsh@gmail.com>
2024-03-05 21:54:50 +0400

    :)

--- dotnet/App.Console/Program.cs
+++ dotnet/App.Console/Program.cs
@@ -1,5 +1,12 @@
 ﻿using Core;
 
-for (int i = 0; i < 1000; i ++){
-    Console.Write(Class1.FizzBuzzer(i) + "_");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write(Class1.FizzBuzzer(i) + " ");
+        }
+    }
 }
